a13b37cfff1dee8771ddb1083bffe5b05ae4152f
Dimitris Mantikidis <dimitrismad@hotmail.com>
2022-03-02 15:47:46 +0200

    FInished the code of all exescises

--- GitRepository/Session-2-Coding-Assessments.cs
+++ GitRepository/Session-2-Coding-Assessments.cs
@@ -39,19 +39,23 @@ public class Session2
         double powa = Math.Pow(6, 4);
         double powb = Math.Pow(5, 7);
         Console.WriteLine((decimal)powa + (decimal)powb / (decimal)9 % 4);
+        Console.WriteLine("");
 
+        //4.
+        Console.WriteLine("4.");
         int age = 20;
         string gender = "female";
         Console.WriteLine("You are "+gender+" and look younger than "+age);
         Console.WriteLine("");
 
-        //4.
-        Console.WriteLine("4.");
+        //5.
+        Console.WriteLine("5.");
         decimal sec = 45678;
         decimal minutes = sec / 60;
         decimal hours = sec / 3600;
         decimal days = sec / 86400;
         decimal years = sec / 31536000;
+        Console.WriteLine("45678 seconds are:");
         Console.WriteLine("Minutes: " + minutes);
         Console.WriteLine("Hours: " + hours);
         Console.WriteLine("Days: " + days);
@@ -59,16 +63,24 @@ public class Session2
         Console.WriteLine("");
 
         //6.
+        Console.WriteLine("6.");
+        int timeinseconds = 45678;
+        TimeSpan Interval = TimeSpan.FromSeconds(timeinseconds);
+        Console.WriteLine("45678 seconds are:");
+        Console.WriteLine((Interval.TotalMinutes) + "Minutes");
+        Console.WriteLine((Interval.TotalHours) + "Hours");
+        Console.WriteLine((Interval.TotalDays) + "Days");
+        Console.WriteLine((Interval.TotalDays / (float)365) + "Years");
         Console.WriteLine("");
 
         //7.
         Console.WriteLine("7.");
         decimal tempc = 27;
-        decimal tempk = tempc * 9 / 5 + 32;
-        //decimal tempf = ;
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
+        decimal tempf = tempc * 9 / 5 + 32;
+        decimal tempk = tempc + 273;
+        Console.WriteLine("If we have 27 Celsius then:");
+        Console.WriteLine("Fahrenheit: "+ tempf);
+        Console.WriteLine("Kelvin: "+tempk);
         Console.WriteLine("");
 
 
