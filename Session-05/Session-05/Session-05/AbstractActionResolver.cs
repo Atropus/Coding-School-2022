758f871bc72478a5eb9a9d68b227e7293eeef376
Dimitris Mantikidis <dimitrismad@hotmail.com>
2022-03-08 16:59:52 +0200

    First very incomplete draft.

---
+++ Session-05/Session-05/Session-05/AbstractActionResolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    public abstract class AbstractActionResolver
+    {
+        public AbstractActionResolver()
+        {
+
+        }
+
+        public abstract string? Execute(string content);
+    }    
+}
---
+++ Session-05/Session-05/Session-05/ActionRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ActionRequest
+    {
+        public Guid RequestID { get; set; }
+
+        public string Input { get; set; } 
+
+        public ActionEnums Action { get; set; }
+
+        public ActionRequest(Guid requestId, string input, ActionEnums action)
+        {
+            RequestID = requestId;
+            Input = input;
+            Action = action;
+        }
+    }
+}
