bac35680dcde6c52006518c09649c43854820d61
Dimitris Mantikidis <dimitrismad@hotmail.com>
2022-03-22 16:30:27 +0200

    Started a new Project for entity framework.

---
+++ S14.EF/Context/S14.Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session_14.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S14.EF.Context
+{
+    internal class Context : DbContext
+    {
+        public DbSet<Prop> Props { get; set; }
+    }
+}
---
+++ Session-14.Model/S14.Prop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_14.Model
+{
+    public class Prop : BaseEntity
+    {
+        public Prop(string title)
+        {
+            Title = title;
+        }
+        public string Title { get; set; }
+        public bool Finished { get; set; }
+
+    }
+}
